3db2d6d24ef4781aa13b6038f9b2f4420802ebdb
parc6502 <ibrahim@serafy.co.uk>
2018-04-18 12:56:07 +0100

    switching from bulma to semantic-ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,33 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { Menu, Image, Icon } from 'semantic-ui-react';
 
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
+  <div className="ui container">
+    <Menu>
+      <Menu.Item as={Link} to="/">
+        <Image src={logo} alt="Kaldi" style={{ width: '88px' }} />
+      </Menu.Item>
+      <Menu.Item as={Link} to="/about">
+        About
+      </Menu.Item>
+      <Menu.Item as={Link} to="/products">
+        Products
+      </Menu.Item>
+      <Menu.Menu position="right">
+        <Menu.Item
           href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
-    </div>
-  </nav>
+            <Icon name="github" alt="Github" />
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  </div>
 )
 
 export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default class IndexPage extends React.Component {
 
     return (
       <section className="section">
-        <div className="container">
+        <div className="ui container">
           <div className="content">
             <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
           </div>
